60210c65625d3daf46bd122e2d6b512e41fc10f6
Filipe Dias <filipediascontato@gmail.com>
2020-11-02 12:23:17 -0300

    Implement number validation

--- src/index.js
+++ src/index.js
@@ -17,23 +17,48 @@ function setBoard () {
 
     for (var i = 0; i < 9; i++) {
         var inputs = rows[i].querySelectorAll('input');
-        console.log(inputs);
         var boardRow = board[i];
 
-        for (var j = 0; j < 9; j++) {
-            if(boardRow[j] === 0) continue; 
-            
-            inputs[j].setAttribute('value', boardRow[j]);
+        for (var j = 0; j < 9; j++) { 
+            if(inputs[j].value == 0) inputs[j].value = '';
+
+            if(boardRow[j] === 0) continue;
+
+            inputs[j].value = boardRow[j];
             inputs[j].setAttribute('class', 'default');
         }
-            
     }
 }
 
-function getBoard() {
+function getBoard () {
+    var rows = document.querySelectorAll('tr');
 
+    for (var i = 0; i < 9; i++) {
+        var inputs = rows[i].querySelectorAll('input');
+        var boardRow = board[i];
+
+        for (var j = 0; j < 9; j++) { 
+            var number = 0;
+            
+            if (inputs[j].value !== null || inputs[j].value !== undefined || inputs[j].value !== NaN) 
+                number = validateNumber(inputs[j].value);
+            
+            boardRow[j] = number;
+        }
+    }
 }
 
 function solve () {
-    
+    getBoard();
+    setBoard();
+
+    console.log('Solving...');
 }
+
+function validateNumber (number) {
+    while (number >= 10)
+        number /= 10;
+    
+    return Math.trunc(number);
+}
+ 
