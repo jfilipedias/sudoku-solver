b62663042ca78999ef0bebd01963d29458ba3071
Filipe Dias <filipediascontato@gmail.com>
2020-11-02 10:27:21 -0300

    Implement a function to setup an initial board

--- src/index.js
+++ src/index.js
@@ -8,13 +8,32 @@ var board = [
     [0, 6, 0, 0, 0, 0, 2, 8, 0],
     [0, 0, 0, 4, 1, 9, 0, 0, 5],
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
+];
+
+setBoard();
 
 function setBoard () {
+    var rows = document.querySelectorAll('tr');
 
+    for (var i = 0; i < 9; i++) {
+        var inputs = rows[i].querySelectorAll('input');
+        console.log(inputs);
+        var boardRow = board[i];
+
+        for (var j = 0; j < 9; j++) {
+            if(boardRow[j] === 0) continue; 
+            
+            inputs[j].setAttribute('value', boardRow[j]);
+            inputs[j].setAttribute('class', 'default');
+        }
+            
+    }
 }
 
 function getBoard() {
 
 }
 
+function solve () {
+    
+}
